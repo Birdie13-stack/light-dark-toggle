a7fa85acffa66b56e6547d95b2b1a253083d2534
sirhmvfx <sirhmvfx@gmail.com>
2023-10-04 00:55:08 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ThemeToggle from "./Components/ThemeToggle";
 
 function App() {
+  const [theme, setTheme] = useState(false);
+
   return (
-    <div className={`App ${darkMode ? "dark-mode" : ""}`}>
+    <div className={theme ? "dark-theme" : "light-theme"}>
       <h1>Hello World</h1>
+
+      <label htmlFor="switch">
+        <input type="checkbox" onChange={() => setTheme(!theme)} />
+      </label>
 
       <ThemeToggle></ThemeToggle>
     </div>
--- src/Components/ThemeToggle.js
+++ src/Components/ThemeToggle.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
 function ThemeToggle() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-
-    console.log(darkMode);
-  };
   return (
-    <div className="theme-toggle">
-      <label>
-        <input type="checkbox" checked={darkMode} onChange={toggleTheme} /> Dark
-        Mode
-      </label>
+    <div>
+      <h1>Something</h1>
     </div>
   );
 }
